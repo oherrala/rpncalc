c175111e57608f3954d65290a794afa7a5456b40
Ossi Herrala <oherrala@gmail.com>
2016-10-08 12:31:49 +0000

    Feature creep: Show prompt, show stack, a bit like calculator interface

--- rpncalc.elm
+++ rpncalc.elm
@@ -10,6 +10,7 @@ main =
 
 type alias Model =
   { stack : Stack
+  , prompt : Maybe Int
   }
 
 type Stack
@@ -18,26 +19,38 @@ type Stack
 
 type Msg
   = Number Int
+  | Enter
   | Addition
   | Subtraction
 
 model : Model
 model =
-  { stack = Empty }
+  { stack = Empty
+  , prompt = Nothing }
 
 update : Msg -> Model -> Model
 update msgs model =
   case msgs of
     Number i ->
-      { stack = push model.stack i }
+      case model.prompt of
+        Nothing ->
+          { model | prompt = Just i }
+        Just n ->
+          { model | prompt = Just (n*10 + i)}
+    Enter ->
+      case model.prompt of
+        Nothing ->
+          model
+        Just i ->
+          { model | stack = push model.stack i, prompt = Nothing }
     Addition ->
-      { stack =
+      { model | stack =
           case stackOp (+) model.stack of
             Nothing -> Empty
             Just s -> s
       }
     Subtraction ->
-      { stack =
+      { model | stack =
           case stackOp (-) model.stack of
             Nothing -> Empty
             Just s -> s
@@ -46,15 +59,51 @@ update msgs model =
 view : Model -> Html Msg
 view model =
   div []
-    [ span [] [ text (toString model) ]
-    , div [] (List.map buttonNum [0..9])
-    , div [] [ button [ onClick Addition ] [ text "+" ]
-             , button [ onClick Subtraction ] [ text "-" ]
-             ]
+    [ pre [] [ viewPrompt model.prompt ]
+    , pre [] (viewStack model.stack)
+    , table [] [ tr [] [ td [] [ buttonNum 7 ]
+                       , td [] [ buttonNum 8 ]
+                       , td [] [ buttonNum 9 ]
+                       , td [] [ buttonAdd ]
+                       ]
+               , tr [] [ td [] [ buttonNum 4 ]
+                       , td [] [ buttonNum 5 ]
+                       , td [] [ buttonNum 6 ]
+                       , td [] [ buttonSub ]
+                       ]
+               , tr [] [ td [] [ buttonNum 1 ]
+                       , td [] [ buttonNum 2 ]
+                       , td [] [ buttonNum 3 ]
+                       ]
+               , tr [] [ td [] [ buttonNum 0 ]
+                       , td [] [ ]
+                       , td [] [ ]
+                       , td [] [ buttonEnter ]
+                       ]
+               ]
     ]
 
-buttonNum n = button [ onClick (Number n) ] [ text (toString n) ]
+viewPrompt prompt =
+  let prefix = "> "
+  in
+    case prompt of
+      Nothing -> text prefix
+      Just i -> text (prefix ++ toString i)
 
+viewStack stack =
+  let viewStack' stack depth =
+    case stack of
+      Empty -> []
+      Item i stack ->
+        let row = toString depth ++ ": " ++ toString i ++ "\n"
+        in text row :: viewStack' stack (depth+1)
+  in
+    viewStack' stack 1
+
+buttonNum n = button [ onClick (Number n) ] [ text (toString n) ]
+buttonEnter = button [ onClick Enter ] [ text "enter" ]
+buttonAdd = button [ onClick Addition ] [ text "+" ]
+buttonSub = button [ onClick Subtraction ] [ text "-" ]
 
 -- Push item into stack
 push : Stack -> Int -> Stack
@@ -76,5 +125,5 @@ stackOp f s1 =
         case pop s2 of
           Nothing -> Nothing
           Just (s3, i2) ->
-            let res = f i1 i2
+            let res = f i2 i1
             in Just (Item res s3)

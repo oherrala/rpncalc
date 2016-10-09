6cf8729c6c4c48b7abfafa180d1f4faf29d294c0
Ossi Herrala <oherrala@gmail.com>
2016-10-09 09:23:44 +0000

    Type variable for Msg and style fix

--- rpncalc.elm
+++ rpncalc.elm
@@ -3,64 +3,87 @@ import Html.App as App
 import Html.Events exposing (..)
 import List as List
 
+
 main =
-  App.beginnerProgram { model = model
-                      , view = view
-                      , update = update }
+  App.beginnerProgram { model = model, view = view, update = update }
+
 
 type alias Model a =
   { stack : Stack a
   , prompt : Prompt a
   }
 
+
 type alias Prompt a = Maybe a
+
 
 type Stack a
   = Item a (Stack a)
   | Empty
 
-type Msg
-  = Number Int
+
+type Msg a
+  = Number a
   | Enter
   | Addition
   | Subtraction
   | Multiplication
 
+
 model : Model a
 model =
   { stack = Empty
-  , prompt = Nothing }
+  , prompt = Nothing
+  }
 
-update : Msg -> Model Int -> Model Int
+
+update : Msg number -> Model number -> Model number
 update msgs model =
   case msgs of
     Number i ->
       case model.prompt of
         Nothing ->
           { model | prompt = Just i }
+
         Just n ->
           { model | prompt = Just (n*10 + i)}
+
     Enter ->
       case model.prompt of
         Nothing ->
           model
+
         Just i ->
           { model | stack = push model.stack i, prompt = Nothing }
+
     Addition ->
       { model | stack = stackOp (+) model.stack }
+
     Subtraction ->
       { model | stack = stackOp (-) model.stack }
+
     Multiplication ->
       { model | stack = stackOp (*) model.stack }
 
-view : Model a -> Html Msg
+
+view : Model number -> Html (Msg number)
 view model =
   let
-    buttonNum n = button [ onClick (Number n) ] [ text (toString n) ]
-    buttonEnter = button [ onClick Enter ] [ text "enter" ]
-    buttonAdd   = button [ onMouseDown Enter, onMouseUp Addition ] [ text "+" ]
-    buttonSub   = button [ onMouseDown Enter, onMouseUp Subtraction ] [ text "-" ]
-    buttonMul   = button [ onMouseDown Enter, onMouseUp Multiplication ] [ text "*" ]
+    buttonNum n =
+      button [ onClick (Number n) ] [ text (toString n) ]
+
+    buttonEnter =
+      button [ onClick Enter ] [ text "enter" ]
+
+    buttonAdd =
+      button [ onMouseDown Enter, onMouseUp Addition ] [ text "+" ]
+
+    buttonSub =
+      button [ onMouseDown Enter, onMouseUp Subtraction ] [ text "-" ]
+
+    buttonMul =
+      button [ onMouseDown Enter, onMouseUp Multiplication ] [ text "*" ]
+
   in
     div []
       [ pre [] [ viewPrompt model.prompt ]
@@ -86,30 +109,41 @@ view model =
                           , td [] [ buttonEnter ]
                           ]
                   ]
-    ]
+        ]
 
-viewPrompt : Prompt a -> Html Msg
+
+viewPrompt : Prompt a -> Html (Msg a)
 viewPrompt prompt =
-  let prefix = "> "
+  let
+    prefix = "> "
   in
     case prompt of
       Nothing -> text prefix
       Just i -> text (prefix ++ toString i)
 
-viewStack : Stack a -> List (Html Msg)
+
+viewStack : Stack a -> List (Html (Msg a))
 viewStack stack =
-  let viewStack' stack depth =
-    case stack of
-      Empty -> []
-      Item i stack ->
-        let row = toString depth ++ ": " ++ toString i ++ "\n"
-        in text row :: viewStack' stack (depth+1)
+  let
+    viewStack' stack depth =
+      case stack of
+        Empty ->
+          []
+
+        Item i stack ->
+          let
+            row = toString depth ++ ": " ++ toString i ++ "\n"
+          in
+            text row :: viewStack' stack (depth+1)
   in
     viewStack' stack 1
 
+
 -- Push item into stack
 push : Stack a -> a -> Stack a
-push stack i = Item i stack
+push stack item =
+  Item item stack
+
 
 -- Pop item from stack
 pop : Stack a -> (Stack a, Maybe a)
@@ -117,6 +151,7 @@ pop stack =
   case stack of
     Empty -> (Empty, Nothing)
     Item i stack -> (stack, Just i)
+
 
 -- Execute operation on stack
 stackOp : (a -> a -> a) -> Stack a -> Stack a
